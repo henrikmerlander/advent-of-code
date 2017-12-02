9d3e622c22fafb844edc2def2e2cbc62b2af422a
Henrik Merlander <henrik.merlander@gmail.com>
2017-12-02 20:58:05 +0100

    Add day 1.1 solution

---
+++ 1/1.1.js
@@ -0,0 +1,13 @@
+let input = require('./readinput')
+
+let sum = 0;
+
+input.forEach((value, index) => {
+    let next = index + 1 >= input.length ?
+        input[0] :
+        input[index + 1];
+
+    if (value == next) sum += value;
+})
+
+console.log(sum);
