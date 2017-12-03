6ecb7aea56bebda349a272ea71206bbe5951c38c
Henrik Merlander <henrik.merlander@gmail.com>
2017-12-03 12:06:10 +0100

    Add day 3.1 solution

---
+++ 3/3.1.js
@@ -0,0 +1,53 @@
+let input = require('./readinput');
+
+let directions = [
+    'RIGHT',
+    'UP',
+    'LEFT',
+    'DOWN'
+]
+
+let getNextDirection = direction => {
+    return direction == 'DOWN' ? directions[0] : directions[directions.indexOf(direction) + 1];
+}
+
+let movePoint = direction => {
+    switch (direction) {
+        case 'RIGHT':
+            point.x++
+            break;
+        case 'UP':
+            point.y++
+            break;
+        case 'LEFT':
+            point.x--
+            break;
+        case 'DOWN':
+            point.y--
+            break;
+        default:
+            break;
+    }
+    step++;
+}
+
+let step = 1;
+let point = { x: 0, y: 0 };
+let steps = 1;
+let direction = 'RIGHT';
+
+(function findTotalSteps() {
+    for (let i = 0; i <= input; i++) {
+        for (let j = 0; j < 2; j++) {
+            for (let k = 0; k < steps; k++) {
+                if (step == input) {
+                    console.log(Math.abs(point.x) + Math.abs(point.y));
+                    return;
+                }
+                movePoint(direction)
+            }
+            direction = getNextDirection(direction);
+        }
+        steps++;
+    }
+})();
