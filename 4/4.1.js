a18452d55dcc42edc1cb26afae6a386820933e0a
Henrik Merlander <henrik.merlander@ezy.se>
2017-12-04 12:55:01 +0100

    Add day 4.1 solution

---
+++ 4/4.1.js
@@ -0,0 +1,8 @@
+let input = require('./readinput');
+
+let valid = input.filter(x => {
+    let words = x.split(' ');
+    return new Set(words).size == words.length;
+})
+
+console.log(valid.length);
