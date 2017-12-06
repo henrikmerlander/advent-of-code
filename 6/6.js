adcae5adb77923951ee8e6a2185bfe5e53cae73b
Henrik Merlander <henrik.merlander@gmail.com>
2017-12-06 22:22:44 +0100

    Add day 6.1 solution

---
+++ 6/6.js
@@ -0,0 +1,22 @@
+let input = require('./readinput')
+
+let states = [];
+let cycles = 0;
+
+while (!states.includes(JSON.stringify(input))) {
+    states.push(JSON.stringify(input));
+
+    let blocks = Math.max(...input);
+    let indexOfHighest = input.indexOf(blocks);
+
+    input[indexOfHighest] = 0;
+
+    for (let index = indexOfHighest + 1; blocks > 0; blocks-- , index++) {
+        if (index >= input.length) index = 0;
+        input[index]++
+    }
+
+    cycles++
+}
+
+console.log(cycles);
