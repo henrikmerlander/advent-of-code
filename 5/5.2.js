5853ab52412f7db6f10a275c8cbf2bbc92d3e7f8
Henrik Merlander <henrik.merlander@ezy.se>
2017-12-06 12:53:38 +0100

    Add day 5.2 solution

---
+++ 5/5.2.js
@@ -0,0 +1,11 @@
+let input = require('./readinput');
+
+let steps = 0;
+
+for (let index = 0; index in input; steps++) {
+    let offset = input[index];
+    input[index] += (offset >= 3 ? -1 : 1);
+    index += offset;
+}
+
+console.log(steps);
