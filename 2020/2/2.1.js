337622f070b890b63919321141b72cbcc751aa01
Henrik Merlander <henrik.merlander@gmail.com>
2020-12-02 07:24:46 +0100

    Solve day 2 2020

---
+++ 2020/2/2.1.js
@@ -0,0 +1,18 @@
+const input = require('fs')
+  .readFileSync('input.txt', 'utf-8')
+  .trim()
+  .split('\n')
+
+const validPasswords = input
+  .map((i) => i.split(' '))
+  .filter((x) => {
+    const min = x[0].split('-')[0]
+    const max = x[0].split('-')[1]
+    const character = x[1][0]
+    const password = x[2]
+    const occurrences = password.split('').filter((c) => c === character).length
+
+    return occurrences >= min && occurrences <= max
+  })
+
+console.log(validPasswords.length)
---
+++ 2020/2/2.2.js
@@ -0,0 +1,20 @@
+const input = require('fs')
+  .readFileSync('input.txt', 'utf-8')
+  .trim()
+  .split('\n')
+
+const validPasswords = input
+  .map((i) => i.split(' '))
+  .filter((x) => {
+    const first = x[0].split('-')[0]
+    const second = x[0].split('-')[1]
+    const character = x[1][0]
+    const password = x[2]
+
+    return (
+      (password.split('')[first - 1] === character) ^
+      (password.split('')[second - 1] === character)
+    )
+  })
+
+console.log(validPasswords.length)
