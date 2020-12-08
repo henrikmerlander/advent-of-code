5cb187c85dff89f536485f58052a62b94695a2b1
Henrik Merlander <henrik.merlander@gmail.com>
2020-12-08 17:23:54 +0100

    Day 8 2020 improvements

--- 2020/8/8.js
+++ 2020/8/8.js
@@ -3,52 +3,55 @@ const input = require('fs')
   .trim()
   .split('\n')
 
-const run = instructions => {
+const program = input
+
+const run = program => {
   let i = 0
   let accumulator = 0
-  let visited = new Set()
+  const visited = new Set()
 
   while (!visited.has(i)) {
     visited.add(i)
 
-    const isTerminated = i === instructions.length
-    if (isTerminated) return { reason: 'Terminated', accumulator }
+    const isTerminated = i === program.length
+    if (isTerminated) return accumulator
 
-    const { groups: { operation, argument } } = /(?<operation>\w\w\w) (?<argument>(\+|-)\d+)/.exec(instructions[i])
+    const [, operation, argument] = /(\w\w\w) ((\+|-)\d+)/.exec(program[i])
 
     switch (operation) {
       case 'nop':
         i++
         break
       case 'acc':
-        accumulator += +argument
+        accumulator += Number(argument)
         i++
         break
       case 'jmp':
-        i += +argument
+        i += Number(argument)
         break
     }
   }
 
-  return { reason: 'Infinite loop', accumulator }
+  throw accumulator
 }
 
-const result = run(input)
-console.log('Part 1', result.accumulator)
+try {
+  run(program)
+} catch (error) {
+  console.log('Part 1', error)
+}
 
-for (let i = 0; i < input.length; i++) {
-  if (!input[i].includes('jmp') && !input[i].includes('nop'))
-    continue
+for (const [index, instruction] of program.entries()) {
+  const [operation] = /(nop|jmp)/.exec(instruction) || []
+  if (!operation) continue
 
-  const instructions = JSON.parse(JSON.stringify(input))
+  const newProgram = Array.from(input)
 
-  instructions[i] = input[i].includes('nop')
-    ? input[i].replace('nop', 'jmp')
-    : input[i].replace('jmp', 'nop')
+  newProgram[index] = instruction.replace(operation, operation === 'nop' ? 'jmp' : 'nop')
 
-  const result = run(instructions)
-  if (result.reason === 'Terminated') {
-    console.log('Part 2', result.accumulator)
+  try {
+    const result = run(newProgram)
+    console.log('Part 2', result)
     break
-  }
+  } catch (error) { }
 }
