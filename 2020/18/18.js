5420c93d20ac0e2a95c29e618fe3867b179d2347
Henrik Merlander <henrik.merlander@gmail.com>
2020-12-18 13:25:41 +0100

    Solve day 18 part 2 2020

--- 2020/18/18.js
+++ 2020/18/18.js
@@ -8,27 +8,38 @@ const calculate = (operand1, operand2, operator) =>
     ? Number(operand1) + Number(operand2)
     : Number(operand1) * Number(operand2)
 
-const evaluateInner = expression =>
-  expression
+const addition = /(\d+) \+ (\d+)/
+
+const evaluateInner = (expression, additionFirst) => {
+  if (additionFirst)
+    while (addition.test(expression))
+      expression = expression.replace(addition, match => eval(match))
+
+  return expression
     .match(/[\+\*]? ?\d+/g)
     .map(r => r.split(' '))
     .reduce((acc, [operator, operand]) => calculate(acc, operand, operator))
-
-const evaluate = expression => {
-  const enclosed = /\([^()]+\)/
-
-  while (enclosed.test(expression)) {
-    const [innerExpression] = enclosed.exec(expression)
-
-    const result = evaluateInner(innerExpression)
-    expression = expression.replace(innerExpression, result)
-  }
-
-  return evaluateInner(expression)
 }
 
-const sum = input
-  .map(evaluate)
-  .reduce((acc, curr) => acc + curr)
+const evaluate = (expression, additionFirst) => {
+  const enclosed = /\([^()]+\)/
 
-console.log('Part 1', sum)
+  while (enclosed.test(expression))
+    expression = expression.replace(
+      enclosed,
+      inner => evaluateInner(inner, additionFirst))
+
+  return evaluateInner(expression, additionFirst)
+}
+
+const sum1 = input
+  .map(expression => evaluate(expression, false))
+  .reduce((acc, curr) => Number(acc) + Number(curr))
+
+console.log('Part 1', sum1)
+
+const sum2 = input
+  .map(expression => evaluate(expression, true))
+  .reduce((acc, curr) => Number(acc) + Number(curr))
+
+console.log('Part 2', sum2)
