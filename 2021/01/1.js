d837036a3df19fe36abc46b2560bcce2c321a17c
Henrik Merlander <henrik.merlander@gmail.com>
2021-12-03 11:08:59 +0100

    Solve day 1 2021

---
+++ 2021/01/1.js
@@ -0,0 +1,12 @@
+const input = require('fs')
+  .readFileSync(`${process.argv[2]}.txt`, 'utf-8')
+  .split('\n')
+  .map(x => parseInt(x))
+
+const result = input.filter((v, i, a) => v > a[i - 1]).length
+
+console.log(result)
+
+const result2 = input.filter((v, i, a) => v > a[i - 3]).length
+
+console.log(result2)
