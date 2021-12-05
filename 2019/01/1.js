c34b3a6369d3a18a21482e746edd87444af2fc6f
Henrik Merlander <henrik.merlander@gmail.com>
2021-12-05 16:00:23 +0100

    Solve day 1 2019

---
+++ 2019/01/1.js
@@ -0,0 +1,24 @@
+const input = require('fs')
+  .readFileSync('input.txt', 'utf-8')
+  .split('\n')
+  .map(Number)
+
+const fuel = weight => Math.floor(weight / 3) - 2
+
+const total = input
+  .map(module => fuel(module))
+  .reduce((acc, curr) => acc + curr)
+
+console.log('Part 1', total)
+
+const totalRecursive = weight => {
+  const fuel = fuel(weight)
+
+  return fuel > 0 ? fuel + totalRecursive(fuel) : 0
+}
+
+const totalRecursive = input
+  .map(module => totalRecursive(module))
+  .reduce((acc, curr) => acc + curr)
+
+console.log('Part 2', totalRecursive)
