36d1f4e68daf9381a3139beb921f2d1834dc0dbb
Henrik Merlander <henrik.merlander@gmail.com>
2022-12-01 11:30:29 +0100

    Solve day 1 2022

---
+++ 2022/01/1.js
@@ -0,0 +1,18 @@
+const input = require('fs')
+  .readFileSync(`${process.argv[2]}.txt`, 'utf-8')
+
+const elves = input
+  .split('\n\n')
+  .map(x => x.split('\n').map(x => parseInt(x)))
+  .map(x => x.reduce((acc, curr) => acc + curr))
+
+const most = Math.max(...elves)
+
+console.log(most)
+
+const topThree = elves
+  .sort((a, b) => b - a)
+  .slice(0, 3)
+  .reduce((acc, curr) => acc + curr)
+
+console.log(topThree)
