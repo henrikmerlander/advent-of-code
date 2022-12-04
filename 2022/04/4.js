b326a1572e9588cb952b07f91628b6a76ce206f1
Henrik Merlander <henrik.merlander@gmail.com>
2022-12-04 15:35:41 +0100

    Solve day 4 2022

---
+++ 2022/04/4.js
@@ -0,0 +1,19 @@
+const input = require('fs')
+  .readFileSync(`${process.argv[2]}.txt`, 'utf-8')
+
+const pairs = input
+  .split('\n')
+  .map(row => {
+    const [, a, b, x, y] = /(\d+)-(\d+),(\d+)-(\d+)/.exec(row)
+    return [Number(a), Number(b), Number(x), Number(y)]
+  })
+
+const contains = pairs
+  .filter(([a, b, x, y]) => a <= x && b >= y || x <= a && y >= b)
+
+console.log(contains.length)
+
+const overlaps = pairs
+  .filter(([a, b, x, y]) => a <= y && b >= x || y <= a && x >= b)
+
+console.log(overlaps.length)
